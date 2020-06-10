76b937f49bc8e4d9bcc84466268c6c65debc54e6
OGFris <ilyesou.cherfaoui.18@gmail.com>
2020-06-10 09:14:31 +0200

    Added remove and create buttons.

--- brenco_keys/lib/pages/home.dart
+++ brenco_keys/lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:brenco_keys/models/key.dart' as LicenseKey;
 import 'package:brenco_keys/providers/api.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:flushbar/flushbar.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,12 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<List<LicenseKey.Key>> futureKey;
+  Future<List<LicenseKey.Key>> futureKeys;
+  List<LicenseKey.Key> _selectedKeys;
+
+  final _nameController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    futureKey = api.fetchKeys();
+    futureKeys = api.fetchKeys();
+    _selectedKeys = [];
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
   }
 
   @override
@@ -28,13 +39,62 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         verticalDirection: VerticalDirection.down,
         children: <Widget>[
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton.icon(
+                icon: Icon(
+                  Icons.add_circle,
+                  color: Theme.of(context).primaryColor,
+                ),
+                label: Text(
+                  'Create',
+                ),
+                onPressed: () {
+                  _addKey(context);
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              Visibility(
+                visible: _selectedKeys.length == 1,
+                child: RaisedButton.icon(
+                  icon: Icon(
+                    Icons.delete,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  label: Text(
+                    'Remove',
+                  ),
+                  onPressed: () {
+                    api.removeKey(_selectedKeys[0].id).then((res) {
+                      setState(() {
+                        _selectedKeys = [];
+                      });
+                    }).catchError((error) {
+                      print(error);
+                      Flushbar(
+                        title: "Hey!",
+                        message: error,
+                        duration: Duration(seconds: 4),
+                      )..show(context);
+                    });
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              )
+            ],
+          ),
           Expanded(
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 SingleChildScrollView(
                     child: FutureBuilder<List<LicenseKey.Key>>(
-                  future: futureKey,
+                  future: futureKeys,
                   builder: (BuildContext context,
                       AsyncSnapshot<List<LicenseKey.Key>> snapshot) {
                     if (snapshot.hasError) {
@@ -116,5 +176,106 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  void _addKey(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            color: Color(0xFF737373),
+            child: Container(
+              decoration: new BoxDecoration(
+                color: Colors.white,
+                borderRadius: new BorderRadius.only(
+                  topLeft: const Radius.circular(10.0),
+                  topRight: const Radius.circular(10.0),
+                ),
+              ),
+              child: Wrap(
+                children: <Widget>[
+                  Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        width: (MediaQuery.of(context).size.width) * 0.5,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal: 16.0,
+                          ),
+                          child: TextField(
+                            controller: _nameController,
+                            autofocus: true,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              hintText: "Name",
+                              hintStyle: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 8.0,
+                      bottom: 8.0,
+                      left: 16.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Icon(
+                          Icons.add_circle,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: Text("Cancel"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                _nameController.clear();
+                              },
+                            ),
+                            FlatButton(
+                              child: Text(
+                                "Save",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                              ),
+                              onPressed: () {
+                                if (_nameController.text.isNotEmpty) {
+                                  api.createKey(_nameController.text).then((_) {
+                                    Navigator.pop(context);
+                                    _nameController.clear();
+                                  }).catchError((error) {
+                                    print(error);
+                                    Flushbar(
+                                      title: "Hey!",
+                                      message: error,
+                                      duration: Duration(seconds: 4),
+                                    )..show(context);
+                                  });
+                                }
+                              },
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- brenco_keys/lib/providers/api.dart
+++ brenco_keys/lib/providers/api.dart
@@ -10,7 +10,7 @@ class API {
   final client = Client();
 
   Future<List<LicenseKey.Key>> fetchKeys() async {
-    List<LicenseKey.Key> keys = List<LicenseKey.Key>();
+    List<LicenseKey.Key> keys = [];
     final response = await client.get("$BASE_URL/api/keys");
 
     if (response.body.startsWith("null")) {
@@ -23,6 +23,28 @@ class API {
       return keys;
     } else {
       throw Exception('Failed to load keys');
+    }
+  }
+
+  Future<bool> createKey(String name) async {
+    final response = await client.post("$BASE_URL/api/keys/create", body: {
+      name: name,
+    });
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception('Failed to create the key, please try again later.');
+    }
+  }
+
+  Future<bool> removeKey(int id) async {
+    final response = await client.post("$BASE_URL/api/keys/remove/$id", body: {});
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      throw Exception('Failed to remove the key, please try again later.');
     }
   }
 }
